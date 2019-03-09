f4aba6f6501f7268e20ba89331159cb7c108f4ac
Foglio1024 <foglio1024@gmail.com>
2019-03-09 09:48:54 +0100

    - add VSH start - refactor

--- index.js
+++ index.js
@@ -1,63 +1,42 @@
-// VS start (VH)
-const VH = {
-    "world" : 9999,
-    "guard" : 1,
-    "section" : 978101
-}
-// VS
-const VS = {
-    "world" : 9999,
-    "guard" : 1,
-    "section" : 9781
-}
-// GG entrance
-const GGVS = {
-    "world" : 9999,
-    "guard" : 1,
-    "section" : 9714
-}
-// GG
-const GG = {
-    "world" : 9999,
-    "guard" : 3,
-    "section" : 9713
-}
-
-module.exports = function vs_location_fix(mod){
+module.exports = function vs_location_fix(mod) {
+    
+    const VH   = { "world": 9999, "guard": 1, "section": 978101 } // VSN start (VH)
+    const VSN  = { "world": 9999, "guard": 1, "section": 9781 }   // VSN
+    const VHH  = { "world": 9999, "guard": 1, "section": 998101 } // VSH start (VH)
+    const VSH  = { "world": 9999, "guard": 1, "section": 9981 }   // VSN
+    const GGVS = { "world": 9999, "guard": 1, "section": 9714 }   // GG entrance
+    const GG   = { "world": 9999, "guard": 3, "section": 9713 }   // GG
 
     mod.hook('S_FRIEND_LIST', 1, (p) => {
         p.friends.forEach(fix);
         return true;
     });
+
     mod.hook('S_GUILD_MEMBER_LIST', 1, (p) => {
         p.members.forEach(fix);
         return true;
     });
 
-    function fix(user){
-        if(isAtGGentrance(user)) fixGG(user);
-        if(isAtVSstart(user)) fixVS(user);
+    function fix(user) {
+        checkAndFix(user, VH, VSN);
+        checkAndFix(user, VHH, VSH);
+        checkAndFix(user, GGVS, GG);
     }
 
-    function isAtVSstart(member){
-        return member.location1 == VH.world &&
-               member.location2 == VH.guard &&
-               member.location3 == VH.section;
+    function checkAndFix(user, loc, correctLoc) {
+        if (isAtLocation(user, loc)) fixLocation(user, correctLoc);
     }
-    function isAtGGentrance(member){
-        return member.location1 == GGVS.world &&
-               member.location2 == GGVS.guard &&
-               member.location3 == GGVS.section;
+
+    function isAtLocation(u, l) {
+        return u.location1 == l.world &&
+            u.location2 == l.guard &&
+            u.location3 == l.section;
     }
-    function fixVS(member){
-        member.location1 = VS.world;
-        member.location2 = VS.guard;
-        member.location3 = VS.section;
-    }
-    function fixGG(member){
-        member.location1 = GG.world;
-        member.location2 = GG.guard;
-        member.location3 = GG.section;
+
+    function fixLocation(u, l) {
+        u.location1 = l.world;
+        u.location2 = l.guard;
+        u.location3 = l.section;
     }
 
 }
